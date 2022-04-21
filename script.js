accbf8a0cb0fc7b47de86583ea7e0874cce78fc9
MikalaiDan <103458690+MikalaiDan@users.noreply.github.com>
2022-04-21 14:10:39 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+"use strict";
+// задания по Основам
+alert ("text");
+let a
+a = 123
+alert(a)
+a = 1+2+3
+alert(a)
+let b = 2
+a = 10
+alert(a+b)
+a = 10
+let d = 5
+let result
+result = a+d
+alert(result)
+a=10
+b = 5
+let c = a-b
+d = 7
+result = c+d
+alert(result)
+a = 13
+b = 5
+alert(a%b)
+let str = "Mikalai"
+alert(str)
+alert(str + " " + "Daneika")
+let str1 = "123"
+alert(str1.length)
+let str2 = "xxx"
+let str3 = "yyy"
+let txt = "aaa ${str2} ${str3} bbb";
+console.log(123)
+let num = 123;
+console.log(num);
+const PI = 3.14;
+// задания по Массивы, Объекты. 
+let arr = ["a","b","c"];
+alert(arr);
+console.log(arr);
+alert(arr[0]);
+alert(arr[1]);
+alert(arr[2]);
+let arr1 = ["a","b","c","d"];
+alert(`${arr1[0]}+${arr1[1]}+${arr1[2]}+${arr1[3]}`);
+let arr2 = [1,2,3];
+alert(arr2[0]+arr2[1]+arr2[2]);
+console.log(arr2.length);
+let obj = {1: 'пн', 2: 'вт', 3: 'ср', 4: 'чт', 5: 'пт', 6: 'сб', 7: 'вс'};
+console.log(obj[1]);
+let obj1 = {a: 1, b: 2, c: 3};
+console.log(obj1["a"]+obj1["b"]+obj1["c"]);
+
+
+
+
+
+
+
+
+
+
