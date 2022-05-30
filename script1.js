6d6ab50c1d2168f3a6b572f614b03f2bf3757e08
MikalaiDan <103458690+MikalaiDan@users.noreply.github.com>
2022-05-30 14:49:37 +0300

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,47 @@
+
+function showCircle(cx, cy, radius) { 
+    let div = document.createElement('div');
+    div.style.width = 0;
+    div.style.height = 0;
+    div.style.left = cx + 'px';
+    div.style.top = cy + 'px';
+    div.className = 'circle';
+    div.append("Стой");
+    document.body.append(div);
+
+    setTimeout(() => {
+      div.style.width = radius * 2 + 'px';
+      div.style.height = radius * 2 + 'px';
+    }, 2000);
+}
+function showCircle1(cx, cy, radius){ 
+    let div = document.createElement('div');
+    div.style.width = 0;
+    div.style.height = 0;
+    div.style.left = cx + 'px';
+    div.style.top = cy + 'px';
+    div.className = 'circle1';
+    div.append("Приготовься");
+    document.body.append(div);
+
+    setTimeout(() => {
+      div.style.width = radius * 2 + 'px';
+      div.style.height = radius * 2 + 'px';
+    }, 2000);
+}
+     function showCircle2(cx, cy, radius){ 
+    let div = document.createElement('div');
+    div.style.width = 0;
+    div.style.height = 0;
+    div.style.left = cx + 'px';
+    div.style.top = cy + 'px';
+    div.className = 'circle2';
+    div.append("ИДИ");
+    document.body.append(div);
+
+    setTimeout(() => {
+      div.style.width = radius * 2 + 'px';
+      div.style.height = radius * 2 + 'px';
+    }, 2000);
+}
+
